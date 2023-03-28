6d3c36fde3864ef9bc73bf51e47ab1ceaf977aed
Patrick <patrick.duputz@hotmail.fr>
2023-03-28 23:06:15 +0200

    up

--- 03 - Math/00-FK_IK/kinematic.h
+++ 03 - Math/00-FK_IK/kinematic.h
@@ -28,13 +28,24 @@ namespace mini_pupper
             float const s2 {  cosf(joint_position[1]) };
             float const c3 { -sinf(joint_position[2]) };
             float const s3 {  cosf(joint_position[2]) };
-
             return Eigen::Vector3f {
                 c1*c3*LEG_LT + c1*c2*LEG_LF - s1*ABDUCTION_OFFSETS[leg_index],
                 s1*c3*LEG_LT + s1*c2*LEG_LF + c1*ABDUCTION_OFFSETS[leg_index],
                   -s3*LEG_LT -    s2*LEG_LF,
             };
         }
+
+        Eigen::Matrix<float,3,4> four_leg_forward_kinematics_LRF(Eigen::Matrix<float,3,4> const & joint_position) const
+        {
+            Eigen::Matrix<float,3,4> lb;
+            lb.col(LEG_FR) = leg_forward_kinematics_LRF(joint_position.col(LEG_FR),LEG_FR);
+            lb.col(LEG_FL) = leg_forward_kinematics_LRF(joint_position.col(LEG_FL),LEG_FL);
+            lb.col(LEG_RR) = leg_forward_kinematics_LRF(joint_position.col(LEG_RR),LEG_RR);
+            lb.col(LEG_RL) = leg_forward_kinematics_LRF(joint_position.col(LEG_RL),LEG_RL);
+            return lb;
+        }
+
+
 
         float LEG_OX {0.059f};
         float LEG_OY {0.0235f};
--- 03 - Math/00-FK_IK/main.cpp
+++ 03 - Math/00-FK_IK/main.cpp
@@ -9,14 +9,29 @@ int main()
 
     mini_pupper::kinematic kin;
 
-    Eigen::Vector3f jp {
-        mini_pupper::to_radians(0.0f),
-        mini_pupper::to_radians(180.0f),
-        mini_pupper::to_radians(90.0f)
-    };
-    Eigen::Vector3f lp { kin.leg_forward_kinematics_LRF(jp,mini_pupper::LEG_FR) };
+    // test 1
+    {
+        Eigen::Vector3f jp {
+            mini_pupper::to_radians(0.0f),
+            mini_pupper::to_radians(180.0f),
+            mini_pupper::to_radians(90.0f)
+        };
+        Eigen::Vector3f lp { kin.leg_forward_kinematics_LRF(jp,mini_pupper::LEG_FR) };
 
-    std::cout << lp << std::endl;
+        std::cout << lp << std::endl;
+    }
+
+    // test 2
+    {
+        Eigen::Matrix<float,3,4> jp {
+            { mini_pupper::to_radians(0.0f),      mini_pupper::to_radians(0.0f),      mini_pupper::to_radians(23.0f),     mini_pupper::to_radians(-15.0f) },
+            { mini_pupper::to_radians(180.0f),    mini_pupper::to_radians(90.0f),     mini_pupper::to_radians(125.0f),    mini_pupper::to_radians(180.0f) },
+            { mini_pupper::to_radians(90.0f),     mini_pupper::to_radians(90.0f),     mini_pupper::to_radians(45.0f),     mini_pupper::to_radians(170.0f) }
+        };
+        Eigen::Matrix<float,3,4> lp { kin.four_leg_forward_kinematics_LRF(jp) };
+
+        std::cout << lp << std::endl;
+    }
 
     return 0;
 }
