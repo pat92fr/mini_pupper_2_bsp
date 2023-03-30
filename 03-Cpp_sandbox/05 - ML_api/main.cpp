4234859ebc2b561f56ccf80a0642d9c1d6c9b8ef
Patrick <patrick.duputz@hotmail.fr>
2023-03-30 22:01:47 +0200

    up

---
+++ 03-Cpp_sandbox/05 - ML_api/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "ml-api.h"
+using namespace mini_pupper;
+
+ml_api ml;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+
+    Eigen::Matrix<float,3,4>joint_position_rad {
+        {0.0f, 0.0f, 0.0f, 0.0f },
+        {0.0f, 0.0f, 0.0f, 0.0f },
+        {0.0f, 0.0f, 0.0f, 0.0f }
+    };
+    ml.set_position(joint_position_rad);
+
+
+
+    return 0;
+}
