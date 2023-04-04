591a92c6a4a8d9b468ea7b8a3c86a3a804034df6
Pat92fr <xxx>
2023-04-04 07:50:10 +0200

    attitude

---
+++ 05-ROS2/ros2_ws/ros2_ws/src/motion/include/mini_pupper.h
@@ -0,0 +1,32 @@
+#ifndef MINI_PUPPER_H_INCLUDED
+#define MINI_PUPPER_H_INCLUDED
+
+namespace mini_pupper
+{
+
+    enum LEG_ID
+    {
+        LEG_FR = 0,
+        LEG_FL = 1,
+        LEG_RR = 2,
+        LEG_RL = 3
+    };
+
+    enum PHASE_STATE
+    {
+        PHASE_STANCE = 0,
+        PHASE_SWING
+    };
+
+};
+
+#include "mini_pupper_config.h"
+#include "mini_pupper_kinematics.h"
+#include "mini_pupper_gait_phase.h"
+#include "mini_pupper_gait_velocity_smoother.h"
+#include "mini_pupper_gait_center_of_curvature.h"
+#include "mini_pupper_gait_leg_trajectory.h"
+#include "mini_pupper_joints.h"
+#include "esp32-api.h"
+
+#endif // MINI_PUPPER_H_INCLUDED
